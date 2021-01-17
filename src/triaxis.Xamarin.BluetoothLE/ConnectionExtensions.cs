6601ef770a2859bf775e248696098eba1100c525
Štefan Šimek <simek@triaxis.sk>
2021-01-17 17:09:47 +0100

    Add option to throw errors when searching for Characteristics

--- src/triaxis.Xamarin.BluetoothLE/ConnectionExtensions.cs
+++ src/triaxis.Xamarin.BluetoothLE/ConnectionExtensions.cs
@@ -15,22 +15,35 @@ namespace triaxis.Xamarin.BluetoothLE
         /// Tries to find the specified characteristic of the specified service
         /// </summary>
         /// <returns>The <see cref="ICharacteristic" /> if found, <see langword="null" /> otherwise.</returns>
-        public static async Task<ICharacteristic> FindServiceCharacteristicAsync(this IPeripheralConnection connection, ServiceUuid service, CharacteristicUuid characteristic)
+        public static async Task<ICharacteristic> FindServiceCharacteristicAsync(this IPeripheralConnection connection, ServiceUuid service, CharacteristicUuid characteristic, bool throwOnError = false)
         {
             var svc = (await connection.GetServicesAsync())?.FirstOrDefault(svc => svc.Uuid == service);
             if (svc == null)
             {
+                if (throwOnError)
+                {
+                    throw new BluetoothLEException($"Service not found: {service}");
+                }
                 return null;
             }
 
-            return (await svc.GetCharacteristicsAsync())?.FirstOrDefault(ch => ch.Uuid == characteristic);
+            var ch = (await svc.GetCharacteristicsAsync())?.FirstOrDefault(ch => ch.Uuid == characteristic);
+            if (ch == null)
+            {
+                if (throwOnError)
+                {
+                    throw new BluetoothLEException($"Characteristic not found: {characteristic} (service {service})");
+                }
+            }
+
+            return ch;
         }
 
         /// <summary>
         /// Tries to find the specified characteristic of the specified service
         /// </summary>
         /// <returns>The <see cref="ICharacteristic" /> if found, <see langword="null" /> otherwise.</returns>
-        public static Task<ICharacteristic> FindServiceCharacteristicAsync(this IPeripheralConnection connection, in ServiceCharacteristicUuid characteristic)
-            => connection.FindServiceCharacteristicAsync(characteristic.Service, characteristic.Characteristic);
+        public static Task<ICharacteristic> FindServiceCharacteristicAsync(this IPeripheralConnection connection, in ServiceCharacteristicUuid characteristic, bool throwOnError = false)
+            => connection.FindServiceCharacteristicAsync(characteristic.Service, characteristic.Characteristic, throwOnError);
     }
 }
